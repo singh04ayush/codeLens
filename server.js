e78a42e4364520ecd2c4a770adbe3135c0a89a97
Ayush Singh <2023505161.ayush@ug.sharda.ac.in>
2026-09-06 00:37:28 +0530

    server setup done

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
+
+const PORT = process.env.PORT
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('CodeLens Server Working')
+})
+
+app.listen(PORT, () => {
+    console.log(`CodeLens Server is running on port ${PORT}`)
+})
